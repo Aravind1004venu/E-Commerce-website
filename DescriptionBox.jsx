41c460f9849ec256d923a5ed5becb464ca69552d
Aravind <142539478+Aravind1004venu@users.noreply.github.com>
2024-08-06 00:00:47 +0530

    Add files via upload

---
+++ DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>E-commerce is the activity of electronically buying or selling products on online services or over the Internet. E-commerce draws on technologies such as mobile commerce, electronic funds transfer, supply chain management, Internet marketing, online transaction processing, electronic data interchange, inventory management systems, and automated data collection systems. E-commerce is the largest sector of the electronics industry and is in turn driven by the technological advances of the semiconductor industry.
+        </p>
+
+        <p>An E-commerce website is a digital platform where buyers and sellers engage in online trading. </p>
+        <p> Here's how it works : A business builds the website, lists the products or services they offer, along with their prices, and connects shoppers to these offerings. Essentially, e-commerce websites provide everything needed for online shopping. These sites facilitate the buying and selling of goods and services, enabling consumers to make purchases conveniently over the internet. </p>
+      </div>
+
+    </div>
+  )
+}
+export default DescriptionBox
